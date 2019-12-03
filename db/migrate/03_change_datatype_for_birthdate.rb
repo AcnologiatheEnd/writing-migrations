f7ba83902238ecc2f0e2442c5c759c4e5bbfbf9b
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-03 22:59:33 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
     change_column (:students, :datetime, :datetime)
